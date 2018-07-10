a13a393c50f9e258b1e94aec266e5005f1e9f42d
darshangit <darshans1989@gmail.com>
2018-07-10 21:08:09 +0530

    navigation + main screen completed

--- App.js
+++ App.js
@@ -2,10 +2,27 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from "react-navigation";
 import { HomeScreenComponent } from "./app/screens/HomeScreen";
+import DayPlanComponent from './app/screens/DayPlanScreen';
+import DayReflectionComponent from './app/screens/DayReflectionScreen';
+import TrendingComponent from './app/screens/TrendingScreen';
+import MotivationalVidComponent from './app/screens/MotivationVidScreen';
+
 const Routes = createStackNavigator({
   HomeScreen: {
     screen: HomeScreenComponent
   },
+  DayPlanScreen: {
+    screen: DayPlanComponent
+  },
+  DayReflectionScreen: {
+    screen: DayReflectionComponent
+  },
+  MotiVidScreen: {
+    screen: MotivationalVidComponent
+  },
+  TrendingScreen: {
+    screen: TrendingComponent
+  }
 },
 {
   initialRouteName: 'HomeScreen'
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,12 +1,73 @@
 import React, { Component } from 'react';
-import {  View, Text, Image , StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Alert
+} from 'react-native';
 
 export class HomeScreenComponent extends Component {
+  static navigationOptions = {
+    header: null
+  };
+
+  onPress = () => {
+    Alert.alert('Button Tapped');
+  };
   render() {
+    const { navigate } = this.props.navigation;
     return (
-      <View>
-        <Image source={require('../assets/mainpage.jpg')}/>
+      <View style={styles.container}>
+        <Image source={require('../assets/mainpage.jpg')} />
+        <View style={styles.subcontainer}>
+          <View style={styles.mainRow}>
+            <TouchableOpacity style={styles.buttonSplit} onPress={() => navigate('DayPlanScreen')}>
+              <Text>DAY PLAN</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonSplit} onPress={() => navigate('DayReflectionScreen')}>
+              <Text>DAY REFLECTION</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.mainRow}>
+            <TouchableOpacity style={styles.buttonSplit} onPress={() => navigate('MotiVidScreen')}>
+              <Text>MOTIVATIONAL VID</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonSplit} onPress={() => navigate('TrendingScreen')}>
+              <Text>TRENDING</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  subcontainer: {
+    flex: 4,
+    backgroundColor: 'lightgrey'
+  },
+  mainRow: {
+    flex: 2,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderColor: 'red',
+  },
+  buttonSplit: {
+    borderColor: 'red',
+    borderRightWidth: 1,
+    borderLeftWidth: 1,
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '50%',
+    height: '50%',
+    backgroundColor: 'white'
+  }
+});
